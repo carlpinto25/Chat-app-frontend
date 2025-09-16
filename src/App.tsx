be737d9573d1676808ddd8d46e7cb5381ad0013a
carl pinto <carlpinto25@gmail.com>
2025-09-16 15:03:37 +0530

    Revert "Loy"

--- src/App.tsx
+++ src/App.tsx
@@ -2,42 +2,49 @@
 import React, { useState } from "react";
 import Sidebar from "./components/Sidebar";
 import ChatWindow from "./components/ChatWindow";
-import ThemeToggle from "./components/Themetoggle";
-import { PanelRightClose, PanelRightOpen } from 'lucide-react';
 
 
 const App: React.FC = () => {
+  
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
-
+  
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
   };
 
   return (
     <div className="flex h-screen dark:bg-gray-600 overflow-y-auto ">
-
+      
       <Sidebar isOpen={isSidebarOpen} />
 
       <div className="flex-1 flex flex-col">
-
-        <div className="p-2 border-b bg-white dark:bg-slate-700 flex justify-between">
+        
+        <div className="p-2 border-b bg-white dark:bg-slate-700">
           <button
             onClick={toggleSidebar}
-            className="p-1 rounded-md hover:bg-gray-200 hover:cursor-pointer dark:hover:bg-gray-800 dark:text-white"
+            className="p-1 rounded-md hover:bg-gray-200 dark:bg-white"
             title="Toggle Sidebar"
           >
-            {isSidebarOpen ? <PanelRightOpen /> : <PanelRightClose />}
-
+            
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
           </button>
-          <div className="dark:text-white font-bold text-lg">
-            AI Chat App
-          </div>
-          <ThemeToggle/>
         </div>
 
-
+        
         <div className="flex-1">
           <ChatWindow />
         </div>
